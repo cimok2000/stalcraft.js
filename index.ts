d02fedc85c711e9ffb21b93213154c7d3b5513f6
cimok2000 <cimok2000@gmail.com>
2023-01-23 01:39:39 +0000

    updated build

--- index.ts
+++ index.ts
@@ -3,13 +3,14 @@ import { IEmission, TFriends, TRegions, TCharacters, IClan, TClanMembers, IClans
 class Stalcraft {
   private demoUrl: string = "https://dapi.stalcraft.net";
   private prodUrl: string = "https://eapi.stalcraft.net";
+  private githubDbUrl: string = "https://api.github.com/repos/EXBO-Studio/stalcraft-database/contents/";
 
   constructor(private appToken: string) { }
 
-  private async request(endpoint: string, method: string = "GET", params?: string, userToken?: string) {
+  private async request(url: string, endpoint: string, method: string = "GET", params?: string, userToken?: string) {
     const requestUrl = params ? `${endpoint}?${params}` : endpoint;
     try {
-      const req = await fetch(`${this.demoUrl}/${requestUrl}`, {
+      const req = await fetch(`${url}/${requestUrl}`, {
         method: method,
         headers: {
           Authorization: userToken ? `Bearer ${userToken}` : `Bearer ${this.appToken}`,
@@ -25,48 +26,53 @@ class Stalcraft {
   }
 
   public async getRegions() {
-    const { data, error } = await this.request(`regions`);
+    const { data, error } = await this.request(this.demoUrl, `regions`);
     return { data: data as TRegions, error };
   }
 
   public async getAuctionHistory(region: string, item: string) {
-    const { data, error } = await this.request(`${region}/auction/${item}/history`);
+    const { data, error } = await this.request(this.demoUrl, `${region}/auction/${item}/history`);
     return { data: data as IAuctionHistory, error };
   }
 
   public async getAuctionLots(region: string, item: string) {
-    const { data, error } = await this.request(`${region}/action/${item}/lots`);
+    const { data, error } = await this.request(this.demoUrl, `${region}/action/${item}/lots`);
     return { data: data as IAuctionLots, error };
   }
 
   public async getCharacters(region: string, userToken: string) {
-    const { data, error } = await this.request(`${region}/characters`, "GET", undefined, userToken);
+    const { data, error } = await this.request(this.demoUrl, `${region}/characters`, "GET", undefined, userToken);
     return { data: data as TCharacters, error };
   }
 
   public async getClan(region: string, clanId: string) {
-    const { data, error } = await this.request(`${region}/clan/${clanId}/info`);
+    const { data, error } = await this.request(this.demoUrl, `${region}/clan/${clanId}/info`);
     return { data: data as IClan, error };
   }
 
   public async getClanMembers(region: string, clanId: string, userToken: string) {
-    const { data, error } = await this.request(`${region}/clan/${clanId}/members`, "GET", undefined, userToken);
+    const { data, error } = await this.request(this.demoUrl, `${region}/clan/${clanId}/members`, "GET", undefined, userToken);
     return { data: data as TClanMembers, error };
   }
 
   public async getClans(region: string) {
-    const { data, error } = await this.request(`${region}/clans`);
+    const { data, error } = await this.request(this.demoUrl, `${region}/clans`);
     return { data: data as IClans, error };
   }
 
   public async getEmission(region: string) {
-    const { data, error } = await this.request(`${region}/emission`);
+    const { data, error } = await this.request(this.demoUrl, `${region}/emission`);
     return { data: data as IEmission, error };
   }
 
   public async getFriends(region: string, character: string, userToken: string) {
-    const { data, error } = await this.request(`${region}/friends/${character}`, "GET", undefined, userToken);
+    const { data, error } = await this.request(this.demoUrl, `${region}/friends/${character}`, "GET", undefined, userToken);
     return { data: data as TFriends, error };
+  }
+
+  public async getItems(region: "global" | "ru") {
+    const { data, error } = await this.request(this.githubDbUrl, `${region}/`);
+    return { data, error };
   }
 }
 
